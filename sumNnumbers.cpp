f3b779ca245a6cd5a6dd1b2d36736df01f76a3e9
Deepak Agrawal <78346232+DeepCoder28@users.noreply.github.com>
2022-02-06 14:15:38 +0530

    Add files via upload

---
+++ sumNnumbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int sum = 0;
+    int i = 1;
+
+    while(i<=n){
+        sum = sum + i;
+        i = i+1;
+    }
+
+    cout<<" Sum of N natural no.s is : "<< sum;
+
+    return 0;
+}
